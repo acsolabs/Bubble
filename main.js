02f8f966741201a9d8ae61cf686eb09ba30ceeab
acsolabs <sofian.achbabi@gmail.com>
2026-03-04 14:10:13 +0100

    feat : add counter score

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // ***********************   BUBBLE CODE ************************
 // **                                                          **
-//Variable
+
+// Variable
 const resume = document.querySelector(".resume");
+const counterDisplay = document.querySelector("h3");
+let count = 0;
 
 // ## Feat : Bubble creation
 
@@ -19,23 +22,34 @@ const addBubble = () => {
   bubble.style.top = Math.random() * 100 + 50 + "%";
   bubble.style.left = Math.random() * 100 + "%";
 
+  //   plusMinus add random 1 || -1  effect calibrate the left position
+  //   const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  //   bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
   bubble.style.setProperty("--left", Math.random() * 100 + "%");
 
   //   color
   const hue = Math.random() * 360;
   const opacity = Math.random() * 0.5 + 0.4;
   bubble.style.background = `hsl(${hue},70%,60%, ${opacity} )`;
+
+  //bubble cleaner
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+
+  //shoot Event
+  bubble.addEventListener("click", () => {
+    bubble.remove();
+    count++;
+    counterDisplay.textContent = count;
+  });
 };
 
-// devmode : to remove before production
-addBubble();
+// bubble generator
+const interval = setInterval(addBubble, 4000);
 
 // ## Feat : Pause
-const interval = setInterval(() => {
-  addBubble();
-}, 1000);
 
 resume.addEventListener("click", (e) => {
   clearInterval(interval);
-  e.stopPropagation();
 });
