c3ce96f6bb91ee4e61ceb6509ce9cda9737ec890
acsolabs <sofian.achbabi@gmail.com>
2026-03-04 10:54:49 +0100

    feat : implement bubble appearing interval & pause button

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-const bubble = document.createElement("span");
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+// ***********************   BUBBLE CODE ************************
+// **                                                          **
+//Variable
+const resume = document.querySelector(".resume");
 
-const bubbleSize = Math.random() * 200 + 100 + "px";
-bubble.style.height = bubbleSize;
-bubble.style.width = bubbleSize;
+console.log(resume);
+// ## Feat : Bubble creation
 
-console.log(bubbleSize);
+const addBubble = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  const bubbleSize = Math.random() * 200 + 100 + "px";
+  bubble.style.height = bubbleSize;
+  bubble.style.width = bubbleSize;
+
+  bubble.style.top = Math.random() * 100 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  console.log(bubble.style.top);
+};
+
+// ## Feat : Pause
+const interval = setInterval(() => {
+  addBubble();
+}, 1000);
+
+resume.addEventListener("click", () => {
+  clearInterval(interval);
+});
